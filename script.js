c0bcf8c5ed29f010ed69d5dd0e9dc08e85308c7b
Vincent <vmgboyles@gmail.com>
2026-05-04 04:36:30 +0800

    Wrote a setGrid function that asks a prompt from user and appends the children squares to appear

---
+++ script.js
@@ -0,0 +1,33 @@
+const box = document.getElementById('box-container');
+// BUTTONS
+const grid = document.getElementById('grid')
+
+
+grid.addEventListener('click', () => {
+    clearGrid()
+    let numOfSquares = +prompt('Enter a number not more than 100');
+    setGrid(numOfSquares)
+})
+
+
+
+
+function setGrid(num) {
+    for (let i = 1; i <= num; i++) {
+        const rectangle = document.createElement('div');
+        rectangle.style.display = 'flex';
+        rectangle.style.flexDirection = 'column';
+        rectangle.style.flex = '1';
+        rectangle.style.flexWrap = 'wrap';
+        box.appendChild(rectangle);
+    for (let j = 1; j <= num; j++) {
+        const squares = document.createElement('div')
+        squares.style.border = '1px solid cornflowerblue';
+        squares.style.width = 'auto'
+        squares.style.height = 'auto'
+        squares.style.flexGrow = '1';
+        rectangle.appendChild(squares)
+    } 
+}
+}
+
