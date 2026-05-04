1921524a397ef509895107064e3123209cd7b6d2
Vincent <vmgboyles@gmail.com>
2026-05-04 23:20:23 +0800

    Added function that randomizes color for event listener mouseover

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const box = document.getElementById('box-container');
 // BUTTON DOMS
 const grid = document.getElementById('grid')
 const clear = document.getElementById('clear');
+let colors = ['#caf0f8', '#ade8f4', '#90e0ef', 
+              '#48cae4', '#00b4d8', '#0096c7'];
+
 
 function setGrid(num) {
+    if (num < 1 || num > 100) {
+        alert('Invalid value! will set it to 16x16!')
+        num = 16
+    }
     for (let i = 1; i <= num; i++) {
         const rectangle = document.createElement('div');
         rectangle.style.display = 'flex';
@@ -17,12 +24,17 @@ function setGrid(num) {
         squares.style.width = 'auto'
         squares.style.height = 'auto'
         squares.style.flexGrow = '1';
-        squares.addEventListener('mouseover', (event) => {
-            squares.style.background = 'blue';
-        })
+        randomColor(squares)
         rectangle.appendChild(squares)
     } 
 }
+}
+
+function randomColor(squares) {
+    let randomColor = Math.round(Math.random() * 6)
+    squares.addEventListener('mouseover', (event) => {
+        squares.style.background = colors[randomColor];
+    })
 }
 
 function clearGrid() {
@@ -32,10 +44,9 @@ function clearGrid() {
 // BUTTON EVENT LISTENERS
 grid.addEventListener('click', () => {
     clearGrid()
-    let numOfSquares = +prompt('Enter a number not more than 100');
-    setGrid(numOfSquares)
+    let num = +prompt('Enter a number not more than 100 and not less than 1');
+    setGrid(num)
 })
 
-clear.addEventListener('click', () => clearGrid())
 
 setGrid(16)
